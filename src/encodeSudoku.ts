87648eec40548824f60cf9c306b197ae50c9c045
George Marcus <george.marcus922@gmail.com>
2024-09-27 02:27:15 +0300

    Add jsdoc

--- src/encodeSudoku.ts
+++ src/encodeSudoku.ts
@@ -1,7 +1,13 @@
 import { encoders } from './formats';
 import { EncodeInput, EncodeResult, SudokuDataFormat } from './types';
 
-// Assumes <constraints> is in <format> format
+/**
+ * Function that encodes puzzle constraints into a data string. The opposite of `decodeSudoku`.
+ * @param {object} params - All parameters are passed as one big object
+ * @param {object} params.constraints - Puzzle constraints. Assumes they are in the `format` format.
+ * @param {SudokuDataFormat} params.format - Constraints format
+ * @returns {EncodeResult} Data string and generated URL
+ */
 export const encodeSudoku = <F extends SudokuDataFormat>({ format, constraints }: EncodeInput<F>): EncodeResult => {
   const encoder = encoders[format]
   return encoder(constraints)
--- src/transformSudoku.ts
+++ src/transformSudoku.ts
@@ -2,7 +2,14 @@ import { compact } from 'lodash-es'
 import { transformers } from './formats'
 import { FormatsToConstraints, SudokuDataFormat, TransformInput, TransformOutput } from './types'
 
-// Assumes <constraints> is in <fromFormat> format
+/**
+ * Function that transforms puzzle constraints from `fromFormat` to `toFormat`
+ * @param {object} params - All parameters are passed as one big object
+ * @param {object} params.constraints - Puzzle constraints. Assumes they are in the `fromFormat` format.
+ * @param {SudokuDataFormat} params.fromFormat - Source format
+ * @param {SudokuDataFormat} params.toFormat - Destination format
+ * @returns {object} Puzzle constraints in the `toFormat` format
+ */
 export const transformSudoku = <FFrom extends SudokuDataFormat, FTo extends SudokuDataFormat>(
   { constraints, fromFormat, toFormat }: TransformInput<FFrom, FTo>
 ): TransformOutput<FormatsToConstraints[FTo]> => {
